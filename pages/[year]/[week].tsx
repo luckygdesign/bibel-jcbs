a34e986d252c12c77775465232aa2b00e1e30f8a
luckygro <lukas@luckyg.de>
2020-07-24 20:45:34 +0200

    add build command

--- pages/[year]/[week].tsx
+++ pages/[year]/[week].tsx
@@ -1,6 +1,7 @@
 import { GetStaticProps, GetStaticPaths } from "next";
 import { getWeekContent, IWeek, getAllWeeks } from "../../lib/week-query";
 import { ParsedMD } from "lib/parseMd";
+import ReactPlayer from "react-player";
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   return {
@@ -46,6 +47,26 @@ function WeekPage(pageProps: { page: IWeek }) {
           md={page?.body || ""}
           className="rose prose-lg text-gray-500 mx-auto"
         />
+        {page?.link && (
+          <div>
+            <h3 className="mt-2 mb-8 text-xl text-center leading-8 font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+              Challenge
+            </h3>
+            <p>
+              Schau dir das Video an und denke darüber nach, wozu Gott alles
+              Leben auf dieser Erde geschaffen hat und was er mit dir persönlich
+              vorhat!
+            </p>
+            <div style={{ paddingTop: "56.25%", position: "relative" }}>
+              <ReactPlayer
+                width="100%"
+                height="100%"
+                style={{ position: "absolute", top: 0, left: 0 }}
+                url={page?.link}
+              />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
